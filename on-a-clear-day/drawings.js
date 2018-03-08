7b3f4b10ac4ab9d1dbcc8c6e9168c0774754e595
Trevor <litseyt@me.com>
2018-03-08 11:17:55 -0600

    added agnes martin on a clear day

---
+++ on-a-clear-day/drawings.js
@@ -0,0 +1,466 @@
+const strokeColor = 'HSLA(342, 0%, 60%, .8)';
+
+const color = {
+	x: strokeColor,
+	y: strokeColor,
+	border: strokeColor,
+}
+
+const width = {
+	x: 1,
+	y: 1,
+	border: 1,
+}
+
+const two = {
+	x: 11,
+	y: 9,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width,
+}
+
+const three = {
+	x: 24,
+	y: 10,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width,
+}
+
+const four = {
+	x: 24,
+	y: 3,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: .8,
+		y: 2,
+		border: 3,
+	}
+}
+
+const five = {
+	x: 24,
+	y: 7,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: .8,
+		y: 2,
+		border: 3,
+	}
+}
+
+
+const six = {
+	x: 0,
+	y: 20,
+	padding: {
+		x: 0,
+		y: 1,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1.2,
+	}
+}
+
+const seven = {
+	x: 0,
+	y: 15,
+	padding: {
+		x: 0,
+		y: 1,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1.2,
+	}
+}
+
+const eight = {
+	x: 0,
+	y: 12,
+	padding: {
+		x: 0,
+		y: 1,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1.2,
+	}
+}
+
+const nine = {
+	x: 0,
+	y: 10,
+	padding: {
+		x: 0,
+		y: 1,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1.2,
+	}
+}
+
+const ten = {
+	x: 0,
+	y: 8,
+	padding: {
+		x: 0,
+		y: 1,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1.2,
+	}
+}
+
+const eleven = {
+	x: 5,
+	y: 10,
+	padding: {
+		x: .8,
+		y: .9,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1,
+		border: 1,
+	}
+}
+
+
+const twelve = {
+	x: 9,
+	y: 13,
+	padding: {
+		x: .8,
+		y: 1,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1,
+		border: 1,
+	}
+}
+
+const thirteen = {
+	x: 3,
+	y: 9,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 2,
+		border: 2,
+	}
+}
+
+const fourteen = {
+	x: 5,
+	y: 13,
+	padding: {
+		x: 1.1,
+		y: 1,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1,
+		border: 1,
+	}
+}
+
+const fifteen = {
+	x: 9,
+	y: 19,
+	padding: {
+		x: .8,
+		y: 1.6,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1,
+		border: 1,
+	}
+}
+
+const sixteen = {
+	x: 6,
+	y: 8,
+	padding: {
+		x: .84,
+		y: .76,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1,
+		border: 1,
+	}
+}
+
+const seventeen = {
+	x: 11,
+	y: 6,
+	padding: {
+		x: 1.8,
+		y: .6,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1,
+	}
+}
+
+const eighteen = {
+	x: 5,
+	y: 19,
+	padding: {
+		x: .4,
+		y: 1.6,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1,
+	}
+}
+
+
+const nineteen = {
+	x: 9,
+	y: 8,
+	padding: {
+		x: .9,
+		y: .7,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1,
+	}
+}
+
+const twenty = {
+	x: 17,
+	y: 8,
+	padding: {
+		x: 1.6,
+		y: .96,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1,
+	}
+}
+
+const twentyOne = {
+	x: 11,
+	y: 8,
+	padding: {
+		x: 1.4,
+		y: .9,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1,
+	}
+}
+
+const twentyTwo = {
+	x: 3,
+	y: 7,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 2,
+	}
+}
+
+const twentyThree = {
+	x: 6,
+	y: 5,
+	padding: {
+		x: .8,
+		y: .6,
+	},
+	color,
+	width: {
+		x: 1,
+		y: 1.2,
+		border: 1.2,
+	}
+}
+
+const twentyFour = {
+	x: 5,
+	y: 7,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1.2,
+		y: 1.4,
+		border: 2.2,
+	}
+}
+
+const twentyFive = {
+	x: 13,
+	y: 20,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1.2,
+		y: 1.4,
+		border: 2.2,
+	}
+}
+
+const twentySix = {
+	x: 9,
+	y: 13,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1.2,
+		y: 1.4,
+		border: 2.2,
+	}
+}
+
+const twentySeven = {
+	x: 18,
+	y: 7,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1.2,
+		y: 1.4,
+		border: 2.2,
+	}
+}
+
+const twentyEight = {
+	x: 8,
+	y: 11,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1.2,
+		y: 1.4,
+		border: 2.2,
+	}
+}
+
+const twentyNine = {
+	x: 5,
+	y: 13,
+	padding: {
+		x: 0,
+		y: 0,
+	},
+	color,
+	width: {
+		x: 1.2,
+		y: 1.4,
+		border: 2.2,
+	}
+}
+
+const paintings = [
+	two,
+	three,
+	four,
+	five,
+	six,
+	seven,
+	eight,
+	nine,
+	ten,
+	eleven,
+	twelve,
+	thirteen,
+	fourteen,
+	fifteen,
+	sixteen,
+	seventeen,
+	eighteen,
+	nineteen,
+	twenty,
+	twentyOne,
+	twentyTwo,
+	twentyThree,
+	twentyFour,
+	twentyFive,
+	twentySix,
+	twentySeven,
+	twentyEight,
+	twentyNine
+]
+
+const getDrawings = () => {
+	return paintings;
+}
+
+
+// w/ padding: 11, 12, 14, 15, 16, 17
